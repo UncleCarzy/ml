144f2f2c26973cc1df075a8d8eab83634fa5e496
ChuahuaLuo <1439638549@qq.com>
2020-02-24 17:24:18 +0800

    master

--- svm/svm1.py
+++ svm/svm1.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class SVC(object):
 
-    def __init__(self, C=1.0, kernel="rbf"):
+    def __init__(self, C=1.0, eps=1e-2, kernel="rbf"):
 
         assert kernel in (
             "rbf", "linear"), "kernel should be one of ('rbf','linear')\n"
@@ -17,14 +17,15 @@ class SVC(object):
         self.support_vector = None
         self.n_sv = None
         self.n_feature = None
-
         # 精度范围
-        self.eps = 1e-8
-
+        self.eps = 0.1
+        self.tol = 1.0
         # 临时变量，训练完会删掉
         self.E = None
         self.K = None
         self.y = None
+
+        self.Done = False
 
     def fit(self, X, y):
         self.n_feature, N = X.shape
@@ -70,7 +71,7 @@ class SVC(object):
             i, j = self.__choose()
 
             ai, aj = self.__solve(i, j)
-
+            self.__object_value_change(ai, aj, i, j, True)
             self.__update(ai, aj, i, j)
 
             if self.__stopping():
@@ -79,7 +80,8 @@ class SVC(object):
     def __initialize(self):
         N = self.y.shape[0]
         self.dual_coef = np.zeros(N)
-        self.intercept = 1.0
+        # self.dual_coef = np.clip(np.random.normal(size=N), 0, self.C)
+        self.intercept = 0.0
         self.E = np.zeros(N)
         for k in range(N):
             self.E[k] = self.__g(k) - self.y[k]
@@ -95,53 +97,95 @@ class SVC(object):
         # 边界约束 0.0 <= alpha <= C
         if not (np.all(0.0 <= self.dual_coef) and np.all(self.dual_coef <= self.C)):
             return False
-
+        print("11")
         # 不等式约束
         N = self.y.shape[0]
         for k in range(N):
-            tmp = self.y[k] * self.__g(k)
-            ak = self.dual_coef[k]
-            if abs(ak) < self.eps and not (tmp >= 1.0):
+            if not self.__check_KKT(k):
                 return False
-            elif (0 < ak and ak < self.C) and not (abs(tmp - 1.0) < self.eps):
-                return False
-            elif abs(ak - self.C) < self.eps and not (tmp <= 1.0):
-                return False
-
         return True
 
     def __choose(self):
         # 选择违背KKT条件最厉害的变量 alpha1, alpha2
+        # 外层循环首先遍历所有 0 < ai < C 的样本点，即在间隔边界上的支持向量点，检验它们是否满足KKT条件
+        # 如果这些样本点都满足KKT条件，那么遍历整个训练集，检验它们是否满足KKT条件
+        best_i, best_j = None, None
         i = 0
         N = self.y.shape[0]
         while i < N:
-            tmp = self.y[i] * self.__g(i)
-            best_i = None
-            ai = self.dual_coef[i]
-            if abs(ai) < self.eps and tmp >= 1.0:
-                best_i = i
-            elif (0.0 < ai and ai < self.C) and abs(tmp - 1.0) < self.eps:
-                best_i = i
-            elif abs(ai - self.C) < self.eps and tmp <= 1.0:
-                best_i = i
+            if self.__check_KKT(i):
+                j = self.__choose_j_by_maxE(i)
+                ai, aj = self.__solve(i, j)
+                if self.__object_value_change(ai, aj, i, j):
+                    return i, j
+                    # best_i, best_j = i, j
+                    # break
 
-            if best_i:
-                best_j = None
-                max_diff_E = 0.0
-                for j in range(N):
-                    if j != best_i:
-                        diff_E = abs(self.E[best_i] - self.E[j])
-                        if diff_E >= max_diff_E:
-                            max_diff_E = diff_E
-                            best_j = j
-                if best_j:
-                    break
+                a = self.dual_coef
+                svp_list = [k for k in range(N) if (k != i and
+                                                    0.0 < a[k] and a[k] < self.C)]
+                for j in svp_list:
+                    ai, aj = self.__solve(i, j)
+                    if self.__object_value_change(ai, aj, i, j):
+                        return i, j
+                        # best_i, best_j = i, j
+                        # break
 
+                nsvp_list = [k for k in range(N) if k != i and not(
+                    0.0 < a[k] and a[k] < self.C)]
+                for j in nsvp_list:
+                    ai, aj = self.__solve(i, j)
+                    if self.__object_value_change(ai, aj, i, j):
+                        return i, j
+                        # best_i, best_j = i, j
+                        # break
             i += 1
-        if 'max_diff_E' not in locals().keys():
-            quit()
-        print("max_diff = %f" % max_diff_E)
+        if best_i == None and best_j == None:
+            best_i, best_j = np.random.choice(N, 2)
+            # raise Exception("选不出合适的i,j\n")
+            # best_j = 1
+            # best_i = 0
+
         return best_i, best_j
+
+    def __choose_j_by_maxE(self, i):
+        N = self.y.shape[0]
+        max_diff_E = 0.0
+        best_j = None
+        for j in range(N):
+            diff = abs(self.E[i] - self.E[j])
+            if j != i and diff > max_diff_E:
+                max_diff_E = diff
+                best_j = j
+        return best_j
+
+    def __object_value_change(self, ai, aj, i, j, flag=False):
+        a = np.copy(self.dual_coef)
+        y = self.y
+        K = self.K
+        old_v = a[i] * y[i] * ((a * y) @ K[i]) + \
+            a[j] * y[j] * ((a * y) @ K[j]) - (a[i] + a[j])
+        a[i], a[j] = ai, aj
+        new_v = a[i] * y[i] * ((a * y) @ K[i]) + \
+            a[j] * y[j] * ((a * y) @ K[j]) - (a[i] + a[j])
+        diff = old_v - new_v
+
+        if flag:
+            print("i = %d j = %d diff = %.5f" % (i, j, diff))
+
+        return diff >= self.tol
+
+    def __check_KKT(self, i):
+        # 检查训练样本(xi,yi)是否满足KKT条件
+        ai = self.dual_coef[i]
+        tmp = self.y[i] * self.__g(i)
+        if abs(ai) < self.eps and not (tmp >= 1.0):
+            return False
+        if 0.0 < ai and ai < self.C and not (abs(tmp - 1.0) < self.eps):
+            return False
+        if abs(ai - self.C) < self.eps and not (tmp <= 1.0):
+            return False
+        return True
 
     def __solve(self, i, j):
         # 解两个变量的子问题
@@ -176,7 +220,9 @@ class SVC(object):
             更新 dual_coef
             更新 E
         """
-        print("a%d = %f\t a%d = %f\n" % (i, ai, j, aj))
+        print("old: a%d = %f\t a%d = %f" %
+              (i, self.dual_coef[i], j, self.dual_coef[j]))
+        print("new: a%d = %f\t a%d = %f\n" % (i, ai, j, aj))
         diff_i = ai - self.dual_coef[i]
         diff_j = aj - self.dual_coef[j]
         intercept_i = - self.E[i] - self.y[i] * self.K[i, i] * \
